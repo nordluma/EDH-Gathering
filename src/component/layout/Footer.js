53c7d186d3e781d59fafc66d73f6125e94f5f711
nordluma <nordluma@arcada.fi>
2020-02-28 15:57:38 +0200

    moved all style into app.css

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -3,24 +3,14 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div style={footerStyle}>
-      <h3 style={linkHeaderStyle}>Links</h3>
+    <div className="footer">
+      <h3>Links</h3>
 
       <Link to="/">Home</Link>
-
-      <h3 style={linkHeaderStyle}>Data Sources</h3>
-
-      <h3 style={linkHeaderStyle}>About</h3>
+      <h3>Data Sources</h3>
+      <h3>About</h3>
     </div>
   );
 }
-
-const footerStyle = {
-  background: "#333"
-};
-
-const linkHeaderStyle = {
-  color: "#fff"
-};
 
 export default Footer;
